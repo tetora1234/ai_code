3df95c9731802dfc38cb8065ace682eb993eb338
tetora1234 <nidera515@gmail.com>
2024-08-23 11:59:44 +0900

    test

---
+++ git_push.bat
@@ -0,0 +1,16 @@
+@echo off
+REM 変更をステージングする
+git add .
+
+REM コミットメッセージを入力
+set /p commitMsg="Enter commit message: "
+
+REM コミットを実行
+git commit -m "%commitMsg%"
+
+REM プッシュを実行
+git push
+
+echo.
+echo Git push completed!
+pause
